4410807ddd60b959d655284e540bbcd98aa44baa
takashi-hashimoto-private <takashi.hashimoto@outlook.com>
2019-04-07 19:44:00 +0900

    add

--- net_use_cmd2/net_use_cmd2/Program.cs
+++ net_use_cmd2/net_use_cmd2/Program.cs
@@ -9,6 +9,19 @@ namespace net_use_cmd2
     {
         static void Main(string[] args)
         {
+
+            string user_name = Environment.UserName;
+
+            System.Diagnostics.Process open_singo06 = new System.Diagnostics.Process();
+            open_singo06.StartInfo.FileName = "cmd.exe";            // コマンド名
+            open_singo06.StartInfo.Arguments = @"net user \\singo06 /user:" + user_name;    // 引数②
+
+            Console.WriteLine(open_singo06.StartInfo.Arguments.ToString());
+
+            open_singo06.StartInfo.CreateNoWindow = true;           // DOSプロンプトの黒い画面を非表示
+            open_singo06.StartInfo.UseShellExecute = false;         // プロセスを新しいウィンドウで起動するか否か
+            open_singo06.StartInfo.RedirectStandardOutput = true;   // 標準出力をリダイレクトで取得したい
+            open_singo06.Start();
         }
     }
 }
